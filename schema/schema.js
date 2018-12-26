231fe49e4dd5e154007afcf7a70f209d73d65ec5
Pob <590650@gmail.com>
2018-12-26 17:47:24 +0700

    add first field

---
+++ schema/schema.js
@@ -0,0 +1,39 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+} = require('graphql')
+const _ = require('lodash')
+
+const books = [
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
+]
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id })
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
