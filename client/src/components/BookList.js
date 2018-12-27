1bf42c10daaf0791b44f2e614daf0c03ed04fbb8
Pob <590650@gmail.com>
2018-12-27 16:48:54 +0700

    added Apollo client boilerplate

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Book Name</li>
+      </ul>
+    )
+  }
+}
+
+export default BookList
